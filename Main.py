e5393aeec8dc6f459665d7b85e786a798aa0c496
BISWAYAN MEHRA <108832092+Biswayan-Mehra@users.noreply.github.com>
2022-09-19 15:56:57 +0530

    biswayan mehra 6

--- Main.py
+++ Main.py
@@ -5,8 +5,7 @@ def display_hash(hashtable) -> None:
 	for i in range(len(hashtable)):
         	print(i, end=" ")
         	for j in hashtable[i]:
-			print("-->", end = " ")
-			print(j, end = " ")
+			print("--> ", j, end=" ")
         	print()
 
 def Hashing(keyvalue) -> int:
